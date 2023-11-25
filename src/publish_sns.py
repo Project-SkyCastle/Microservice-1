a0f9623742869533921cca6e10e932ad075abece
Mengyuan Huang <mehuang@student.unimelb.edu.au>
2023-11-25 10:41:35 -0500

    Update publish_sns.py

--- src/publish_sns.py
+++ src/publish_sns.py
@@ -5,6 +5,9 @@ def send_sns_message(message, message_attributes):
 
     sns = boto3.client('sns',
                        region_name='us-east-1',
+                       aws_access_key_id="ASIAZCJCIC74WP65OBUK",
+                       aws_secret_access_key="WYGZhLv2uBqzRQt/MVnjc2VgBgXwspMJBZN3T1Xm",
+                       aws_session_token="FwoGZXIvYXdzEDEaDCCyXSmSJjrAMShaiyLEAWZnbubkUblLN7rLtlU8P/7psNBFMK0ZFpQBUblkA2/POrH+pXME97A+Q2u1b4YEZt3KAYIhQ87+vvlHoJQde5IhOudm0ijsgI3TszlWGhHa4mYRr8hlgtByPKqXboFQOvdw4e9eccwJqpVRSCmcI5aahPHW+EjLyv4tUMyAg+UVbU1fMWOIEUwB0wp1CzTf1yi1OOwu2KHQ0TGh7NMof9PerkoG6z8tKA5IY04oDyhsuzcWMAoxw09iTn4+EzurC4ivVSgoj5yIqwYyLeBeJpYrnPVbJzjtxaXpSceiYElLB1UpraEy6kxbYwhvFAJiqjqDqi74OAmduA=="
                        )
 
 
