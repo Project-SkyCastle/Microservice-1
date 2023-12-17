33fdcfff720389c6a26f94448efaba67bcb92c6b
sp3949 <sp3949@columbia.edu>
2023-12-17 12:35:30 -0500

    Modify script

--- restart.sh
+++ restart.sh
@@ -5,8 +5,7 @@ if [ ! -z "${pid}" ]; then
     kill $pid
 fi
 
-cd ~/6156Project_SkyCastle-Microservice1/ &&
-git pull -f origin main && python3 -m virtualenv .venv && source .venv/bin/activate &&  python -m pip install -r requirements.txt && python main.py
+cd ~/6156Project_SkyCastle-Microservice1/ && git pull -f origin main && python3 -m pip install -r requirements.txt && python3 main.py
 
 echo "successfully restarted service."
 
